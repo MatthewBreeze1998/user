232ffd4f1fb990da713e05454edea45f82ee378b
BREEZE <T7077260@live.tees.ac.uk>
2019-11-04 16:38:36 +0000

    Add project files.

---
+++ Could-System-dev-ops/Controllers/StaffController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Could_System_dev_ops.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Could_System_dev_ops.Controllers
+{
+    public class StaffController : Controller
+    {
+
+
+
+        private StaffModel _context;
+        public StaffController(StaffModel context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// The Create
+        /// </summary>
+        /// <param name="staff">The staff<see cref="Staff"/></param>
+        /// <returns>The <see cref="Task{IActionResult}"/></returns>
+        [HttpPost]
+        public async Task<IActionResult> AddStaff([Bind("StaffId,FirstName,LastName,Email,ContactNumebr,PayRoll")]StaffModel staff)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(staff);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(staff);
+        }
+       
+    }
+}
