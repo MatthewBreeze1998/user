e4816dc85219ce26e0eabf3f3d98220a759d7deb
MATTHEW BREEZE <T7077260@live.tees.ac.uk>
2019-11-23 22:05:37 +0000

    review  fake

---
+++ Could-System-dev-ops/Models/ReviewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Could_System_dev_ops.Models
+{
+    public class ReviewModel
+    {
+      
+        public int ProductId { get; set; }
+        
+        public String Review { get; set; }
+
+   
+
+
+
+    }
+}
