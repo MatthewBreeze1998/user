a7b7955525ab312679ea535c9211b1a504dbb90e
MATTHEW BREEZE <T7077260@live.tees.ac.uk>
2019-12-08 20:24:28 +0000

    db conextfile

---
+++ Could-System-dev-ops/Models/UserDataBaseContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Could_System_dev_ops.Models
+{
+    public class UserDataBaseContext : DbContext
+    {
+
+        public DbSet<UsersModel> User { get; set; }
+
+
+        public UserDataBaseContext(DbContextOptions<UserDataBaseContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+
+
+
+
+
+    }
+}
